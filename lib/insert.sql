e68b7716f912c5e4c4afaa688389008535e516cd
Charlie Cook <turingbombe@gmail.com>
2016-08-03 17:02:21 -0400

    Finalized bear organizer

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) 
+VALUES 
+  (1,"Mr.Chocolate", 1, "M", "Brown", "Delicious", 0),
+  (2,"Rowdy", 15, "F", "Burgundy", "Raucous", 1),
+  (3,"Tabitha", 6, "M", "Silver", "Magical", 1),
+  (4,"Sargeant Brown", 20, "M", "Salt & Pepper", "Wise", 1),
+  (5,"Melissa", 17, "F", "Blone", "Sullen", 0),
+  (6,"Grinch", 3, "M", "Green", "Humbug", 1),
+  (7,"Wendy", 10, "F", "Ginger", "Koalaesque", 1),
+  (8,null, 2, "M", "Burgundy", "Hungry", 0);
